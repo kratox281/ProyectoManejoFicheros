a9fa388851350f3ac24feaccb280d3fa6aff3527
kratox281 <rcmartinez281@gmail.com>
2023-01-04 12:33:01 +0100

    Lectura y escritura xlsx

---
+++ src/main/java/lecturaEscrituraXLSX/LecturaYEscritura.java
@@ -0,0 +1,111 @@
+package lecturaEscrituraXLSX;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class LecturaYEscritura {
+	// Variables necesarias tanto como para leer como escribir el fichero por medio
+	// de la libreria Apache POI
+	private File archivo;
+	private FileInputStream fis;
+	private XSSFWorkbook libro;
+	private XSSFSheet hoja;
+
+	public LecturaYEscritura() {
+		try {
+			archivo = new File("/ManejoFicheros/Archivos/datos.gob.es/Defunciones.xlsx");
+			System.out.println(archivo.getAbsolutePath().toString());
+			fis = new FileInputStream(archivo);
+			libro = new XSSFWorkbook(fis);
+			hoja = libro.getSheetAt(0);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	//Metodo al que llama el jsp para realizar la lectura del fichero
+	public Map<Integer, Object[]> lectura() {
+		
+		//Mapa para almacenar los datos
+		Map<Integer, Object[]> datos = new HashMap<>();
+		Iterator<Row> filas = hoja.iterator();
+
+		// Iteramos para obtener los datos dato de cada fila
+		int contadorFilas = 0;
+
+		while (filas.hasNext()) {
+
+			contadorFilas++;
+			Object[] valores = new Object[3];
+			Row fila = filas.next();
+
+			Iterator<Cell> celdas = fila.iterator();
+			int contadorCeldas = 0;
+			// Obtenemos los datos de cada columna
+			while (celdas.hasNext()) {
+
+				contadorCeldas++;
+
+				Cell celda = celdas.next();
+
+				switch (celda.getCellType()) {
+				case Cell.CELL_TYPE_STRING:
+					// Añadir los datos al mapa para despues imprimirlo en el jsp
+					valores[contadorCeldas] = celda.getStringCellValue();
+					// Comprobacion
+					//System.out.print(celda.getStringCellValue() + "\t");
+					break;
+				case Cell.CELL_TYPE_NUMERIC:
+
+					valores[contadorCeldas] = celda.getNumericCellValue();
+					// Comprobacion
+					//System.out.println(celda.getNumericCellValue() + "\t");
+					break;
+				case Cell.CELL_TYPE_BOOLEAN:
+
+					valores[contadorCeldas] = celda.getBooleanCellValue();
+					// Comprobacion
+					//System.out.println(celda.getBooleanCellValue() + "\t");
+					break;
+				}
+			}
+			//Insertamos en el mapa
+			datos.put(contadorFilas, valores);
+		}
+
+		return datos;
+
+	}
+
+	public void escritura(ArrayList<String> datos) {
+		try {
+		 int numeroFilas = hoja.getLastRowNum();
+		 //Creamos una fila nueva
+		 Row fila = hoja.createRow(numeroFilas++);
+		 int numeroCelda  = 0;
+		 for(String dato:datos) {
+			 //Creamos una celda para cada dato
+			 Cell celda = fila.createCell(numeroCelda++);
+			 celda.setCellValue((String)dato);
+		 }
+		 //Escribimos en el archivo los cambioss
+		 FileOutputStream fos = new FileOutputStream(archivo);
+         libro.write(fos);
+         //Comprobacion
+         //System.out.println("Escrito con exito");
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+}
--- src/main/webapp/DatosAbiertosXLS.jsp
+++ src/main/webapp/DatosAbiertosXLS.jsp
@@ -1,5 +1,8 @@
+<%@page import="java.util.Map.Entry"%>
+<%@page import="lecturaEscrituraXLSX.LecturaYEscritura"%>
+<%@page import="java.util.*"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +10,64 @@
 <title>Insert title here</title>
 </head>
 <body>
+	<%!LecturaYEscritura lYE = new LecturaYEscritura();%>
+	<%!String eleccion;%>
 
+	<%
+	if (request.getParameter("opcion") != null) {
+		eleccion = request.getParameter("opcion");
+	} else {
+		eleccion = "nulo";
+	}
+	%>
+
+	<!-- Aqui comienzan los metodos de escritura -->
+	<%
+	if (eleccion.equals("escritura")) {
+	%>
+	<%!ArrayList<String> datosNuevos;%>
+	<%
+	//Recoge los datos insertados en la web de acceso
+	if (request.getServletContext().getAttribute("datos") != null) {
+		datosNuevos = (ArrayList<String>) request.getServletContext().getAttribute("datos");
+	} else {
+		datosNuevos = new ArrayList();
+		datosNuevos.add("dato 1");
+		datosNuevos.add("dato 2");
+		datosNuevos.add("dato 3");
+	}
+
+	//Llama al metodo en la clase externa para escribir los datos en el fichero
+	lYE.escritura(datosNuevos);
+	%>
+	<!--Se indica al usuario que su peticion de registro de datos se ha realizado correctamente y que se le mostrará a continueació -->
+	<p>Se ha registrado con exito,se le mostrará el documento
+		resultante</p>
+	<%
+	}
+	%>
+	<!-- Aqui acaba la escritura debajo de esta linea está la lectura que la hace por defecto para demostrar que se han registrado los nuevos datos -->
+	<%!Map<Integer, Object[]> mapa;%>
+	<table border="3">
+		<%
+		mapa = lYE.lectura();
+		Set<Integer> set = mapa.keySet();
+
+		for (Integer clave : set) {
+		%>
+		<tr>
+			<%
+			Object[] datos = mapa.get(clave);
+			for (Object dato : datos) {
+			%>
+			<td><%=dato.toString()%></td>
+			<%
+			}
+			%>
+		</tr>
+		<%
+		}
+		%>
+	</table>
 </body>
 </html>
